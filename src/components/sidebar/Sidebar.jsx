5c974ee41a7c64fa7f9c0f97a0a2eb0bd9e9c88b
Jaime1401 <jaime14albalate@gmail.com>
2023-09-30 14:27:22 +0200

    closable desktop

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,54 +1,41 @@
 'use client'
 
 import { TeamMember } from '@/components/TeamMember'
-import { useState, useEffect } from 'react'
 
 import { SidebarHeader } from './SidebarHeader'
 import { SidebarNavigation } from './SidebarNavigation'
 import { SidebarTeams } from './SidebarTeams'
-import { Search } from './Search'
 
-export function Sidebar ({ navigation, teams, collapsable = false }) {
-  const [open, setOpen] = useState(false)
 
-  useEffect(() => {
-    const down = (e) => {
-      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
-        e.preventDefault()
-        setOpen((open) => !open)
-      }
-    }
+export function Sidebar ({ navigation, teams, sheet = false, setSidebarOpen, sidebarOpen, setSearchOpen }) {
+  if (sidebarOpen || sheet) {
+    return (
+      <>
+        <div className="h-full z-50 flex w-72 flex-col">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex grow flex-col gap-3 overflow-y-auto border-r border-gray-200 bg-white px-6 py-4">
 
-    document.addEventListener('keydown', down)
-    return () => document.removeEventListener('keydown', down)
-  }, [])
+            <SidebarHeader setOpenSearchCommand={setSearchOpen} setSidebarOpen={setSidebarOpen} sheet={sheet} />
 
-  return (
-    <>
-      <div className="h-full z-50 flex w-72 flex-col">
-        {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex grow flex-col gap-1.5 overflow-y-auto border-r border-gray-200 bg-white px-4 pb-4">
+            <nav className="flex flex-1 flex-col">
+              <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                <li>
+                  <SidebarNavigation navigation={navigation} />
+                </li>
+                <li>
+                  <SidebarTeams teams={teams} />
+                </li>
+                <li className="mt-auto">
+                  <TeamMember />
+                </li>
+              </ul>
+            </nav>
 
-          <SidebarHeader setOpenSearchCommand={setOpen} collapsable={collapsable} />
-
-          <nav className="flex flex-1 flex-col">
-            <ul role="list" className="flex flex-1 flex-col gap-y-7">
-              <li>
-                <SidebarNavigation navigation={navigation} />
-              </li>
-              <li>
-                <SidebarTeams teams={teams} />
-              </li>
-              <li className="mt-auto">
-                <TeamMember />
-              </li>
-            </ul>
-          </nav>
-
+          </div>
         </div>
-      </div>
-
-      <Search open={open} setOpen={setOpen} />
-    </>
-  )
+      </>
+    )
+  }
+  
+  return null
 }
--- src/components/sidebar/SidebarHeader.jsx
+++ src/components/sidebar/SidebarHeader.jsx
@@ -2,9 +2,9 @@ import { SheetClose } from '../ui/sheet'
 import { Button } from '@/components/ui/button'
 import { LuPanelLeft } from 'react-icons/lu'
 
-export function SidebarHeader ({ collapsable = false, setOpenSearchCommand }) {
+export function SidebarHeader ({ sheet = false, setOpenSearchCommand, setSidebarOpen }) {
   return (
-    <div className="flex h-16 justify-between items-center gap-1.5">
+    <div className="flex flex-row-reverse justify-between items-center gap-3">
       <Button variant="outline" onClick={ () => setOpenSearchCommand((open) => !open) } className=" hover:text-indigo-600 flex justify-between items-center w-full rounded-md border-0 text-gray-400
       shadow-sm ring-1 focus:shadow-sm sm:text-sm sm:leading-6 px-2 py-1 focus:ring-gray-300 ring-gray-300">
         <p>Search...</p>
@@ -12,14 +12,19 @@ export function SidebarHeader ({ collapsable = false, setOpenSearchCommand }) {
           <span className="text-xs">⌘</span>K
         </kbd>
       </Button>
-      {collapsable &&
+      {sheet ? (
         <SheetClose asChild>
-          <Button variant="ghost" size="icon" className="hover:text-indigo-600 group">
+          <Button variant="ghost" size="icon" className="hover:text-indigo-600 group px-4 py-4">
             <span className="sr-only">Close sidebar</span>
             <LuPanelLeft className="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600" />
           </Button>
         </SheetClose>
-        }
+      ) : (
+          <Button variant="ghost" size="icon" className="hover:text-indigo-600 group px-4 py-4" onClick={ () => setSidebarOpen(false) }>
+            <span className="sr-only">Close sidebar</span>
+            <LuPanelLeft className="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600" />
+          </Button>
+        )}
     </div>
   )
 }
