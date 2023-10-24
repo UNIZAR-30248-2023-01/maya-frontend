efea86aa992e079a173bd0156a81a7d8d144d62d
carlotaquintana <796017@unizar.es>
2023-10-24 16:53:04 +0200

    crear rama

--- src/app/[lang]/in-and-outs/page.js
+++ src/app/[lang]/in-and-outs/page.js
@@ -10,6 +10,6 @@ export default function InAndOutsPage () {
     <>
       <div className='border-2 border-dashed min-h-full flex items-center justify-start h-24 w-full p-4'/>
       <div className='border-2 border-dashed min-h-full flex items-center justify-start h-96 w-full p-4'/>
-    </>
+    </> 
   )
 }
