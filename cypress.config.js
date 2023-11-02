fa447db9eec6011dd408026c34475cf05b884f0c
Hector Toral <hectortoraltrabajos@gmail.com>
2023-11-02 17:16:53 +0100

    fix: prueba de variables de entorno

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,10 @@ require('dotenv').config()
 module.exports = defineConfig({
   video: false,
   screenshotOnRunFailure: false,
-  env: { ...process.env },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY || ''
+  },
   e2e: {
     baseUrl: 'http://localhost:3000'
   }
