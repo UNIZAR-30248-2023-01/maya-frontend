b151000e462bf207e3dc65eb6fbcc1627a1fa5c2
alberto01hc <109821771+alberto01hc@users.noreply.github.com>
2023-11-06 09:38:20 +0000

    Obtener username

--- src/app/[lang]/home/page.js
+++ src/app/[lang]/home/page.js
@@ -1,8 +1,35 @@
-export default function home () {
+'use client'
+import React, { useEffect, useState } from 'react';
+import { devolverTokenUsuario } from '@/lib/jwt_sign_verify'
+
+export default function Home() {
+  // State para almacenar el valor de la cookie
+  const [cookieValue, setCookieValue] = useState('');
+
+  // Función para obtener el valor del token
+  function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(name + '=')) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+    return null;
+  }
+
+  // Efecto para obtener y mostrar el valor de la cookie cuando el componente se monta
+  useEffect(() => {
+    const token = getCookie("OutsiteJWT");
+    if (token) {
+      const valor = devolverTokenUsuario(token, "secret") //DEVUELVE ID USUARIO
+      setCookieValue(valor);
+    }
+  }, []);
+
   return (
-    <>
-      <div className='border-2 border-dashed min-h-full flex items-center justify-start h-24 w-full p-4'/>
-      <div className='border-2 border-dashed min-h-full flex items-center justify-start h-96 w-full p-4'/>
-    </>
-  )
+    <div>
+      <p>Bienvenido: {cookieValue || 'No se encontró la cookie'}</p>
+    </div>
+  );
 }
--- src/lib/jwt_sign_verify.ts
+++ src/lib/jwt_sign_verify.ts
@@ -1,3 +1,5 @@
+'use client'
+
 import { SignJWT, jwtVerify, type JWTPayload } from 'jose';
 
 export async function sign(payload, secret: string): Promise<string> {
@@ -15,7 +17,25 @@ export async function sign(payload, secret: string): Promise<string> {
 export async function verify(token: string, secret: string): Promise<JWTPayload> {
     const { payload } = await jwtVerify(token, new TextEncoder().encode(secret));
     // run some checks on the returned payload, perhaps you expect some specific values
-
     // if its all good, return it, or perhaps just return a boolean
     return payload;
 }
+
+export async function devolverTokenUsuario(token, secret) {
+  
+    try {
+      const payload: JWTPayload = await verify(token, secret);
+      // Haz lo que necesites con el objeto payload aquí
+      const payloadString = JSON.stringify(payload);
+      const userIdMatch = payloadString.match(/"userId":"([^"]+)"/);
+
+    if (userIdMatch && userIdMatch[1]) {
+      const userId = userIdMatch[1];
+      return userId;
+    } else {
+      return null;
+    }
+    } catch (error) {
+      console.error('Error al verificar el token:', error);
+    }
+  }
