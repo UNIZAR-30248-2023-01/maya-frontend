d09d7ee8516be81c86f6c0caac4dd0a317edb5b4
Hector Toral <hectortoraltrabajos@gmail.com>
2023-12-19 13:09:50 +0100

    fix: variable de entorno

--- src/components/settings/password-form.jsx
+++ src/components/settings/password-form.jsx
@@ -29,7 +29,7 @@ export function PasswordForm () {
         return new Promise((resolve, reject) => {
           (async () => {
             if (form.password !== null && pssword === form.password) {
-              await fetch(`${process.env.VERCEL_URL}/api/change-password`, {
+              await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api/change-password`, {
                 method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ password: form.password })
